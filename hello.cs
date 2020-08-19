acacf6f84d7a7928f77fc3a5ee891509682447c1
1170442029 <69881561+1170442029@users.noreply.github.com>
2020-08-19 11:30:32 +0800

    Revert "Revert "Create hello.cs""
    This reverts commit 51c88574101375c911834ccbd7702402502aac44.

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+namespace GitHUB
+{
+    class GitHUB
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello c#");
+
+            int a= 30;
+        } 
+    }
+}
